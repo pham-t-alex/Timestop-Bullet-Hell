f1a56264514b90236d5b3e0aa5a7912dcdad0a46
pham-t-alex <pham.t.alex@gmail.com>
2024-10-15 01:05:12 -0700

    salvo firing delay

--- Scenes/boss.gd
+++ Scenes/boss.gd
@@ -1,27 +1,29 @@
 extends CharacterBody2D
 
 @export var action_set: Array[SalvoData]
-var salvoStack: Array[SalvoData]
-var salvoDelayStack: Array[int]
+var currently_executing_salvo := false
 
 signal generate_bullet(pos: Vector2, data: BulletData)
+signal completed_action
 
 func _ready() -> void:
-	_generate_bullets_from_salvo(action_set[0])
+	take_action()
 
-func _generate_bullets_from_salvo(salvo: SalvoData) -> void:
+func _generate_bullets_from_salvo(salvo: SalvoData, topLevel: bool) -> void:
 	for data in salvo.shots:
 		if data is SalvoData:
-			_generate_bullets_from_salvo(data as SalvoData)
+			_generate_bullets_from_salvo(data as SalvoData, false)
 		else:
 			var bullet_data := data as BulletData
 			generate_bullet.emit(position, bullet_data)
+	$SalvoDelay.start(salvo.delay_after_fire)
+	await $SalvoDelay.timeout
+	if topLevel:
+		completed_action.emit()
 
-func _on_bullet_delay_timeout() -> void:
-	if salvoStack.is_empty():
-		var rng := RandomNumberGenerator.new()
-		var index := rng.randi_range(0, len(action_set) - 1)
-		salvoStack.push_back(action_set[index])
-	var salvo := salvoStack.pop_back() as SalvoData
-	
-	_generate_bullets_from_salvo(salvo as SalvoData)
+func take_action() -> void:
+	var rng := RandomNumberGenerator.new()
+	var index := rng.randi_range(0, len(action_set) - 1)
+	currently_executing_salvo = true
+	var salvo := action_set[index]
+	_generate_bullets_from_salvo(salvo as SalvoData, true)
